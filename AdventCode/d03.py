8778362f7644ed9ad7112f202759f6264495e7d1
oleksandraluch <oluchakova@myseneca.ca>
2023-12-18 21:59:01 -0500

    Day 3 puzzle 1

--- AdventCode/d03.py
+++ AdventCode/d03.py
@@ -1,10 +1,29 @@
-# Day 3, p1
+# Day 3
 
-def read_adjacent_numbers(line, adjacent_indexes):
-    for symbol in line:
-        pass
+# Returns sum of the numbers in line that whose indexes are in adjacent_indexes
+# number is considered to be adjacent even if one digit has adjacent index
+def sum_adjacent_numbers(line, adjacent_indexes):
+    total_sum = 0
+    reading_num = False
+    is_adjacent = False
+    curr_num = ""
 
-# returns indexes where adjacent numbers can appear in the previous or next line
+    for index, symbol in enumerate(line):
+        if symbol.isnumeric() and not reading_num:
+            curr_num = symbol
+            reading_num = True
+            if index in adjacent_indexes: is_adjacent = True
+        elif symbol.isnumeric() and reading_num:
+            curr_num += symbol
+            if index in adjacent_indexes: is_adjacent = True
+        elif not symbol.isnumeric():
+            if curr_num != "" and is_adjacent: 
+                total_sum += int(curr_num)
+            reading_num = False
+            is_adjacent = False
+    return total_sum
+        
+# Returns indexes where adjacent numbers can appear in the previous or next line
 def get_adjacent_indexes(line):
     symbols = "!@#$%^&*()_-+={}[]/"
     adj_indexes = []
@@ -22,15 +41,34 @@ def get_adjacent_indexes(line):
             if index < len(line) - 1:
                 if not (index+1) in adj_indexes: 
                     adj_indexes.append(index+1)
-    adj_indexes.sort()
     return adj_indexes
 
-if __name__ == "__main__":
+# Puzzle 1 solution
+def sum_all_adjacent_numbers(filename="d03_input.txt"):
 
-    ifile = open("d03_smoke.txt", "r")
+    ifile = open(filename, "r")
     lines = ifile.readlines()
+    sum_adjacent = 0
 
-    for line in lines:
-        print(get_adjacent_indexes(line))
+    for index, line in enumerate(lines):
+        # Find indexes that might store adjacent numbers
+        adjacent_indexes = [] # gonna be unsorted, duplicates possible
+        # get indexes from the prev line
+        if index > 0:
+            adjacent_indexes.extend(get_adjacent_indexes(lines[index - 1]))
+        # get indexes from the line
+        adjacent_indexes.extend(get_adjacent_indexes(line))
+        # get indexes from the next line
+        if index < len(lines) - 2:
+            adjacent_indexes.extend(get_adjacent_indexes(lines[index + 1]))
 
-    
+        # if want to remove duplicates and sort:
+        # adjacent_indexes = list(dict.fromkeys(adjacent_indexes))
+        # adjacent_indexes.sort()
+            
+        sum_adjacent += sum_adjacent_numbers(line, adjacent_indexes)
+    ifile.close()
+    return sum_adjacent
+
+if __name__ == "__main__":
+    print(f"Puzzle 1: {sum_all_adjacent_numbers('d03_input.txt')}")
--- AdventCode/tests.py
+++ AdventCode/tests.py
@@ -3,6 +3,7 @@
 import unittest
 from d01 import puzzle1, puzzle2
 from d02 import sum_valid_games, sum_powers_of_games, games
+from d03 import get_adjacent_indexes, sum_all_adjacent_numbers
 
 class TestDay01(unittest.TestCase):
 
@@ -19,6 +20,20 @@ class TestDay02(unittest.TestCase):
 
     def test_puzzle2(self):
         self.assertEqual(sum_powers_of_games(games), 55593)
+
+class testDay03(unittest.TestCase):
+
+    def test_get_adjacent_indexes(self):
+
+        self.assertEqual(get_adjacent_indexes("...."), [])
+
+        self.assertEqual(get_adjacent_indexes(""), [])
+
+        self.assertEqual(get_adjacent_indexes("83@.*."), [2, 1, 3, 4, 5])
+
+        self.assertEqual(get_adjacent_indexes("..83@.*.951*..984*111..-@"), [4, 3, 5, 6, 7, 11, 10, 12, 17, 16, 18, 23, 22, 24])
+
+        self.assertEqual(sum_all_adjacent_numbers(), 528819)
 
 if __name__ == "__main__":
     unittest.main()

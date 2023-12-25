b130c37814304ad850f59582860f31af7a8f3838
oleksandraluch <oluchakova@myseneca.ca>
2023-12-25 17:55:02 -0500

    Added decodeWays solution and tests

---
+++ oleksandra/decodeWays.py
@@ -0,0 +1,28 @@
+# Problem: https://leetcode.com/problems/decode-ways/description/?envType=daily-question&envId=2023-12-25
+
+# Constraints:
+#     1 <= s.length <= 100
+#     s contains only digits and may contain leading zero(s).
+
+
+#def numDecodings(self, s):
+def numDecodings(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    decodeWays = 0
+    valid_nums = list(range(1, 27))
+
+    # check if the first digit is 0
+    if s[0] == '0': return decodeWays # if the first digit is 0, there's no way to decode s
+
+    for index, curr_num in enumerate(s):
+        if int(curr_num) in valid_nums:
+            decodeWays += 1
+            # append next num to curr_num and validate it
+            if index < len(s) - 1:
+                curr_num += s[index + 1]
+                if curr_num in valid_nums: 
+                    decodeWays += 1
+    return decodeWays
--- oleksandra/tests.py
+++ oleksandra/tests.py
@@ -2,11 +2,22 @@
 
 import unittest
 from medianOfSortedArrays import findMedianSortedArrays
+from decodeWays import numDecodings
 
 class test_medianOfSortedArrays(unittest.TestCase):
     def test_medianOfSortedArrayss(self):
         self.assertEqual(findMedianSortedArrays([3,4], [1,2]), 2.5)
         self.assertEqual(findMedianSortedArrays([1,3], [2]), 2)
+
+    def test_decodeWays(self):
+        self.assertEqual(numDecodings("12"), 2)
+        self.assertEqual(numDecodings("226"), 3)
+        self.assertEqual(numDecodings("06"), 0)
+        self.assertEqual(numDecodings("0088"), 0)
+        self.assertEqual(numDecodings("46"), 2)
+        
+
+
 
 if __name__ == "__main__":
     unittest.main()

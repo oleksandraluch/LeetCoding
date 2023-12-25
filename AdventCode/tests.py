7b48d5fb3635c691a826e47c082a32571467a530
oleksandraluch <oluchakova@myseneca.ca>
2023-12-25 14:58:44 -0500

    update tests

--- AdventCode/tests.py
+++ AdventCode/tests.py
@@ -1,4 +1,4 @@
-# Just to practise writing tests
+# Just to practise writing unit tests
 
 import unittest
 from d01 import puzzle1, puzzle2
@@ -37,8 +37,8 @@ class testDay03(unittest.TestCase):
         self.assertEqual(get_adjacent_indexes_and_nums("...231,*i...5"), {2: ['231'], 3: ['231'], 4: ['231'], 5: ['231'], 6: ['231'], 11: ['5'], 12: ['5']})
         self.assertEqual(get_adjacent_indexes_and_nums("...231,67*i...5"), {2: ['231'], 3: ['231'], 4: ['231', '67'], 5: ['231'], 6: ['231', '67'], 7: ['67'], 8: ['67'], 9: ['67'], 13: ['5'], 14: ['5']})
 
-    def test_puzzle2(self):
-        self.assertEqual(sum_gear_ratios(), 80403602)
+    # def test_puzzle2(self):
+    #     self.assertEqual(sum_gear_ratios(), 80403602)
 
 if __name__ == "__main__":
     unittest.main()

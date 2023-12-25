317d73eda976cbf4fb85b9180c9f55cce2afb1e2
oleksandraluch <oluchakova@myseneca.ca>
2023-12-25 18:03:52 -0500

    update vars namings

--- oleksandra/decodeWays.py
+++ oleksandra/decodeWays.py
@@ -11,18 +11,18 @@ def numDecodings(s):
     :type s: str
     :rtype: int
     """
-    decodeWays = 0
+    decode_ways = 0
     valid_nums = list(range(1, 27))
 
     # check if the first digit is 0
-    if s[0] == '0': return decodeWays # if the first digit is 0, there's no way to decode s
+    if s[0] == '0': return decode_ways # if the first digit is 0, there's no way to decode s
 
     for index, curr_num in enumerate(s):
         if int(curr_num) in valid_nums:
-            decodeWays += 1
+            decode_ways += 1
             # append next num to curr_num and validate it
             if index < len(s) - 1:
                 curr_num += s[index + 1]
                 if curr_num in valid_nums: 
-                    decodeWays += 1
-    return decodeWays
+                    decode_ways += 1
+    return decode_ways

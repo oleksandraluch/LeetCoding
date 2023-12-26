6e1b629111d597bd2ac3f256a6c036cac5872237
oleksandraluch <oluchakova@myseneca.ca>
2023-12-25 22:10:23 -0500

    update lengthOfLongestSubstring()

--- oleksandra/longest_substring.py
+++ oleksandra/longest_substring.py
@@ -7,24 +7,6 @@
 #     0 <= s.length <= 5 * 104
 #     s consists of English letters, digits, symbols and spaces.
 
-
-# def lengthOfLongestSubstring(s):
-#     """
-#     :type s: str
-#     :rtype: int
-#     """
-
-#     longest_substr = ""
-#     curr_substr = ""
-
-#     for symbol in s:
-#         if not symbol in curr_substr:
-#             curr_substr += symbol
-#         else:
-#             if len(curr_substr) > len(longest_substr): longest_substr = curr_substr
-
-#     return longest_substr
-
 def lengthOfLongestSubstring(s):
     """
     :type s: str
@@ -36,16 +18,13 @@ def lengthOfLongestSubstring(s):
 
     for index, leading in enumerate(s):
         curr_substr = leading
-        if index < len(s) - 1:
-            for symbol in s[index:]:
-                if not symbol in curr_substr:
-                    curr_substr += symbol
-                else:
-                    if len(curr_substr) > len(longest_substr): longest_substr = curr_substr
-                    curr_substr = ""
+        for symbol in s[index+1:]:
+            if not symbol in curr_substr:
+                curr_substr += symbol
+            else:
+                if len(curr_substr) > len(longest_substr): longest_substr = curr_substr
+                curr_substr = ""
+
     print(longest_substr)
     return len(longest_substr)
 
-if __name__ == "__main__":
-    print(lengthOfLongestSubstring("abcabcbb"))
-    print(lengthOfLongestSubstring("9908"))
